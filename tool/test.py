77b5f43bc988bb4c2fe40e01810349cad009d44a
s04240051 <59446793+s04240051@users.noreply.github.com>
2022-03-18 18:24:32 +0100

    Update test.py
    fit fusion and aux

--- tool/test.py
+++ tool/test.py
@@ -4,20 +4,23 @@ import os
 import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
-
+from lib.data_build.data_utils import loss_weight
 
 import lib.model.two_stream as model_set
 from lib.data_build import Data_loader
-from lib.config_file import cfg
-import lib.model.utils as utils
+#import lib.model.utils as utils
+import lib.utils.logging as log
+import lib.utils.checkpoint as cu
+from lib.utils.meter import Test_meter
+import lib.utils.solver as sol
 
-logger = utils.get_logger(__name__)
+logger = log.get_logger(__name__)
 
 
 def build_model(cfg):
     model = eval(f"model_set.{cfg.MODEL.MODEL_NAME}(cfg).cuda()")
 
-    test_meter = utils.test_meter(cfg)
+    test_meter = Test_meter(cfg)
     return (
         model,
         test_meter,
@@ -25,7 +28,7 @@ def build_model(cfg):
 
 
 def test_net(cfg):
-    utils.setup_logging(cfg.OUT_DIR)
+    log.setup_logging(cfg.OUT_DIR)
     (
         model,
         test_meter,
@@ -34,21 +37,21 @@ def test_net(cfg):
     data_container = Data_loader(cfg)
     logger.info("start load dataset")
 
-    test_loader = data_container.construct_loader("test")
-    labels = (np.array(test_loader.dataset.label_list)[:, -1]).astype(int)
-    test_epoch = utils.load_test_checkpoint(cfg, model)
+    test_loader, loss_weight = data_container.construct_loader("test")
+    #labels = (np.array(test_loader.dataset.label_list)[:, -1]).astype(int)
+    test_epoch = cu.load_test_checkpoint(cfg, model)
     cudnn.benchmark = True
 
     logger.info("Testing model for {} iterations".format(len(test_loader)))
 
-    val_epoch(cfg, model, test_loader, test_meter, labels, test_epoch)
+    val_epoch(cfg, model, test_loader, test_meter, test_epoch)
 
 
 @torch.no_grad()
-def val_epoch(cfg, model, test_loader, test_meter, label_set, epoch=-1):
+def val_epoch(cfg, model, test_loader, test_meter, epoch=-1):
     model.eval()
     test_meter.time_start()
-    for cur_iter, (inputs, labels) in enumerate(tqdm(test_loader)):
+    for cur_iter, (inputs, labels, start_name) in enumerate(tqdm(test_loader)):
         test_meter.time_pause()
         test_meter.update_data()
         test_meter.time_start()
@@ -62,28 +65,33 @@ def val_epoch(cfg, model, test_loader, test_meter, label_set, epoch=-1):
         labels = labels.cuda()
         labels = labels.unsqueeze(-1)
         outputs = model(inputs)
-
-        loss_func = nn.BCEWithLogitsLoss().cuda()
+        if cfg.MODEL.MODEL_NAME == "Two_stream_fusion" and cfg.MODEL.FUSION_METHOD == "late":
+            loss_func = nn.BCELoss().cuda()
+        else:
+            loss_func = nn.BCEWithLogitsLoss().cuda()
         loss = loss_func(outputs, labels.float())
 
         if cfg.MODEL.NUM_LABELS == 2:
-            acc = utils.get_binary_acc(outputs, labels)
+            acc = sol.get_binary_acc(outputs, labels)
         else:
-            acc = utils.get_accuracy(outputs, labels)
+            acc = sol.get_accuracy(outputs, labels)
 
         loss, acc = (
             loss.item(),
             acc.item(),
         )
         batch_size = inputs[0].size(0)
+        outputs = torch.sigmoid(outputs)
         outputs = outputs.cpu()
-        test_meter.update_states(loss, acc, batch_size, outputs)
+        labels  = labels.cpu()
+        
+        test_meter.update_states(loss, acc, batch_size, outputs, labels, start_name)
 
         test_meter.time_pause()
         test_meter.update_batch()
         test_meter.time_start()
 
-    test_meter.update_epoch(epoch, cfg, label_set)
+    test_meter.update_epoch(epoch)
     acc = test_meter.acc_meter.avg
     f1_score = test_meter.f1
     test_meter.reset()
@@ -91,8 +99,8 @@ def val_epoch(cfg, model, test_loader, test_meter, label_set, epoch=-1):
 
 
 def test_full(cfg):
-    utils.setup_logging(cfg.OUT_DIR)
-    checkpoints_set = utils.get_checkpoints_set(cfg)
+    log.setup_logging(cfg.OUT_DIR)
+    checkpoints_set = cu.get_checkpoints_set(cfg)
     assert len(checkpoints_set) > 0, f"no checkpoints file avalible in {cfg.CHECKPOINTS_FOLD}"
     
     (
