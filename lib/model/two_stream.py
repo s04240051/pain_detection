b82fe8523af6a5bf4924172b8d2a6073b0b1ba1c
s04240051 <59446793+s04240051@users.noreply.github.com>
2022-03-18 18:04:54 +0100

    Update two_stream.py
    Adding Flow_stream, Two_stream_fusion models

--- lib/model/two_stream.py
+++ lib/model/two_stream.py
@@ -1,7 +1,8 @@
+from turtle import forward
 import torch.nn as nn
 import torch
 from .conv_LSTM import ConvLSTM
-from .model_utils import TemporalAttn, TimeDistributed, Head
+from . import model_utils as utils
 import torch.nn.functional as F
 
 
@@ -10,13 +11,19 @@ class Two_stream_model(nn.Module):
 
         super(Two_stream_model, self).__init__()
         self.clstm = Img_stream(
-            cfg.MODEL.CLSTM_HIDDEN_SIZE, cfg.MODEL.NUM_CLSTM_LAYERS, cfg.MODEL.IMG_SIZE
+            cfg.MODEL.CLSTM_HIDDEN_SIZE, 
+            cfg.MODEL.NUM_CLSTM_LAYERS, 
+            cfg.MODEL.IMG_SIZE,
+            with_attention=cfg.MODEL.ATTENTION,
         )
         self.lstm_stream = Kp_lstm(
-            cfg.MODEL.LSTM_INPUT_SIZE, cfg.MODEL.NUM_LSTM_LAYERS, cfg.MODEL.LSTM_HIDDEN_SIZE
+            input_size= cfg.MODEL.LSTM_INPUT_SIZE, 
+            num_layers= cfg.MODEL.NUM_LSTM_LAYERS, 
+            hidden_size= cfg.MODEL.LSTM_HIDDEN_SIZE,
+            with_attention=cfg.MODEL.ATTENTION,
         )
         fuse_dim = self.clstm.linear_input + cfg.MODEL.LSTM_HIDDEN_SIZE
-        self.head = Head(cfg, fuse_dim)
+        self.head = utils.Head(cfg, fuse_dim)
         
 
     def forward(self, x):
@@ -27,11 +34,69 @@ class Two_stream_model(nn.Module):
         f_k_fuse = torch.concat([frame, kp], 1)
         final_out = self.head(f_k_fuse)
         return final_out
+class Two_stream_fusion(nn.Module):
+    def __init__(self, cfg):
+        super(Two_stream_fusion, self).__init__()
+        self.clstm = Img_stream(
+            cfg.MODEL.CLSTM_HIDDEN_SIZE, 
+            cfg.MODEL.NUM_CLSTM_LAYERS, 
+            cfg.MODEL.IMG_SIZE,
+            with_attention=cfg.MODEL.ATTENTION,
+        )
+        self.lstm_stream = Kp_lstm(
+            input_size= cfg.MODEL.LSTM_INPUT_SIZE, 
+            num_layers= cfg.MODEL.NUM_LSTM_LAYERS, 
+            hidden_size= cfg.MODEL.LSTM_HIDDEN_SIZE,
+            with_attention=cfg.MODEL.ATTENTION,
+        )
+        image_dim = self.clstm.linear_input
+        kp_dim = cfg.MODEL.LSTM_HIDDEN_SIZE
+        self.fusion_method = eval(f"utils.{cfg.MODEL.FUSION_METHOD}(image_dim, kp_dim, cfg)")
+    
+    def forward(self, x):
+        frame = x[0]
+        kp = x[1]
+        frame = self.clstm(frame)
+        kp = self.lstm_stream(kp)
+        final_out = self.fusion_method(frame, kp)
+        return final_out
+
+class Flow_model(nn.Module):
+    def __init__(self, cfg):
+        super(Flow_model, self).__init__()
+        nb_units = cfg.MODEL.CLSTM_HIDDEN_SIZE
+        nb_layers = cfg.MODEL.NUM_CLSTM_LAYERS
+        input_width, input_height = cfg.MODEL.IMG_SIZE
+        self.rgb_stream = ConvLSTM(
+            3, nb_units, (5,5), nb_layers, True, True, False
+        )
+        self.flow_stream = ConvLSTM(
+            3, nb_units, (5,5), nb_layers, True, True, False
+        )
+        self.linear_input = (input_height//(2**nb_layers)) * \
+            (input_width//(2**nb_layers))*nb_units
+        
+        self.timedistribute = utils.TimeDistributed(nn.Flatten(1))
+        self.attention = utils.TemporalAttn(self.linear_input)
+        self.head = utils.Head(cfg, self.linear_input)
+    def forward(self, x):
+        rgb = x[0]
+        flow = x[1]
+        rgb = self.rgb_stream(rgb)[0][0]
+        flow = self.flow_stream(flow)[0][0]
+        fuse = torch.add(rgb, flow)
+        fuse = self.timedistribute(fuse)
+        fuse, weight = self.attention(fuse)
+        out = self.head(fuse)
+        return out
 class Rgb_model(nn.Module):
     def __init__(self, cfg):
         super(Rgb_model, self).__init__()
         self.clstm = Img_stream(
-            cfg.MODEL.CLSTM_HIDDEN_SIZE, cfg.MODEL.NUM_CLSTM_LAYERS, cfg.MODEL.IMG_SIZE
+            cfg.MODEL.CLSTM_HIDDEN_SIZE, 
+            cfg.MODEL.NUM_CLSTM_LAYERS, 
+            cfg.MODEL.IMG_SIZE,
+            with_attention=cfg.MODEL.ATTENTION,
         )
         out_units = 1 if cfg.MODEL.NUM_LABELS == 2 else cfg.MODEL.NUM_LABELS
         self.fc = nn.Linear(self.clstm.linear_input, out_units)
@@ -47,11 +112,13 @@ class Img_stream(nn.Module):
         nb_layers,
         input_size,
         nb_labels=2,
+        with_attention=True,
         top_layer=False,
     ):
 
         super(Img_stream, self).__init__()
         self.top_layer = top_layer
+        self.with_attention = with_attention
         self.convlstm = ConvLSTM(
             3, nb_units, (5, 5), nb_layers, True, True, False)
         input_width, input_height = input_size
@@ -59,16 +126,20 @@ class Img_stream(nn.Module):
             (input_width//(2**nb_layers))*nb_units
         out_units = 1 if nb_labels == 2 else nb_labels
         self.fc = nn.Linear(self.linear_input, out_units)
-        self.timedistribute = TimeDistributed(nn.Flatten(1))
-        
-        self.attention = TemporalAttn(self.linear_input)
+        self.timedistribute = utils.TimeDistributed(nn.Flatten(1))
+        self.flat = nn.Flatten(1)
+        self.attention = utils.TemporalAttn(self.linear_input)
 
     def forward(self, x):
-        x = self.convlstm(x)[0][0]  # (batch, time, hidden_size, w, h)
-
-        x = self.timedistribute(x)
-
-        x, weight = self.attention(x)  # (batch, hidden_size)
+        output, h = self.convlstm(x)  # (batch, time, hidden_size, w, h)
+        if self.with_attention:
+            x = output[0]
+            x = self.timedistribute(x)
+            x, weight = self.attention(x)  # (batch, hidden_size)
+        else:
+            x = h[0][0]
+            x = self.flat(x)
+        
         if self.top_layer:
             x = self.fc(x)
             
@@ -84,10 +155,12 @@ class Kp_lstm(nn.Module):
         num_layers,
         hidden_size,
         nb_labels=2,
+        with_attention=True,
         top_layer=False,
     ):
 
         super(Kp_lstm, self).__init__()
+        self.with_attention = with_attention
         self.top_layer = top_layer
         self.lstm = nn.LSTM(
             input_size=input_size,
@@ -95,12 +168,15 @@ class Kp_lstm(nn.Module):
             num_layers=num_layers,
             batch_first=True,
         )
-        self.attention = TemporalAttn(hidden_size)
+        self.attention = utils.TemporalAttn(hidden_size)
         self.fc = nn.Linear(hidden_size, nb_labels)
 
     def forward(self, x):
         x, (h_n, c_n) = self.lstm(x)
-        x, weights = self.attention(x)
+        if self.with_attention:
+            x, weights = self.attention(x)
+        else:
+            x = h_n.permute(1,0,2)[:,-1,:]
         if self.top_layer:
             out = self.fc(x)
             return out
