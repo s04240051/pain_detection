ddf83a1fabbb6c881732478a7be3b9c2afd78ca6
s04240051 <59446793+s04240051@users.noreply.github.com>
2022-03-18 18:31:39 +0100

    add best_policy

--- lib/utils/checkpoint.py
+++ lib/utils/checkpoint.py
@@ -1,6 +1,10 @@
 import os
 import torch
+import torch.nn as nn
+import lib.utils.logging as log
+from collections import OrderedDict
 
+logger = log.get_logger(__name__)
 def load_train_checkpoint(cfg, model, optim):
     
     file_list = get_checkpoints_set(cfg)
@@ -8,7 +12,7 @@ def load_train_checkpoint(cfg, model, optim):
     if cfg.TRAIN_INITIAL_WEIGHT != "":
         file_path = cfg.TRAIN_INITIAL_WEIGHT
         assert os.path.isfile(
-            cfg.TEST_INITIAL_WEIGHT
+            cfg.TRAIN_INITIAL_WEIGHT
         ), "Checkpoint '{}' not found".format(file_path) 
 
     elif file_list:
@@ -40,14 +44,40 @@ def load_test_checkpoint(cfg, model):
         ), "Checkpoint '{}' not found".format(file_path) 
     else:
         print(
-            "Unknown way of loading checkpoint. Using with random initialization, only for debugging."
+            "Unknown way of loading checkpoint."\
+            "Using with random initialization, only for debugging."
         )
         return 
     checkpoint = torch.load(file_path)
     test_epoch  = checkpoint["epoch"]
-    model.load_state_dict(checkpoint['model_state'])
+    model_dict = model.state_dict()
+    new_dict = {k:v for k,v in checkpoint["model_state"].items() if k in model_dict.keys()}
+    model_dict.update(new_dict)
+    model.load_state_dict(model_dict)
     return test_epoch
+
+def load_ft(file, model, fine_tune_layer=[]):
+    assert os.path.isfile(file), "no checkpoints file found"
+    pre_dict = torch.load(file)
+    model_dict = model.state_dict()
+    new_dict = {k: v for k, v in pre_dict.items() if k in model_dict.keys()}
+    model_dict.update(new_dict)
+    model.load_state_dict(model_dict)
     
+    paraname_set = []
+    for prefix in fine_tune_layer:
+        for name, para in model.named_parameters():
+            if name.startswith(prefix):
+                print('  Finetuning parameter: {}'.format(name))
+                paraname_set.append(name)
+    for name, para in model.named_parameters():
+        if name not in paraname_set:
+            para.requires_grad = False
+    if not fine_tune_layer:
+        for name, module in model.named_modules():
+            if isinstance(module, nn.BatchNorm3d):
+                module.training = False
+
 def save_checkpoint(out_dir, checkpoints_fold, model, optim, cur_epoch, num_gpus):
     sd = model.module.state_dict() if num_gpus > 1 else model.state_dict()
     checkpoint = {
@@ -80,7 +110,7 @@ def get_checkpoints_set(cfg):
         return []
 
 def save_policy(cur_epoch, isbest, cfg):
-    if isbest and cur_epoch > cfg.SOLVER.WARMUP_EPOCHS:
+    if isbest:
         return True
     save_step = cfg.RUN.SAVE_STEP
     if cur_epoch >= cfg.SOLVER.MAX_EPOCH-cfg.RUN.SAVE_LAST:
@@ -89,3 +119,20 @@ def save_policy(cur_epoch, isbest, cfg):
         return True
     else:
         return False
+class best_policy:
+    def __init__(self, cfg):
+        if (cfg.SOLVER.LR_POLICY).startswith("steps"):
+            self.mark_start = cfg.SOLVER.STEPS[1]
+        else:
+            self.mark_start = cfg.WARMUP_EPOCHS
+        self.best_pred = 0
+        self.isbest = False
+        self.best_epoch = 0
+    def update(self, cur_epoch, acc):
+        if cur_epoch >= self.mark_start and acc > self.best_pred:
+            self.best_pred = acc
+            self.best_epoch = cur_epoch
+            return True
+        else:
+            return False
+        
