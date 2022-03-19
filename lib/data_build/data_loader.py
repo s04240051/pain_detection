564fa53075532bd4146bc87d9a9641fa26a044a7
s04240051 <59446793+s04240051@users.noreply.github.com>
2022-03-19 17:39:02 +0100

    add flow load, data loader in kinetics format change crop method

--- lib/data_build/data_loader.py
+++ lib/data_build/data_loader.py
@@ -14,17 +14,13 @@ class Data_loader:
         if cfg.FORMAT_DATASET and cfg.DATASET_PATH != "":
             data_path = cfg.DATASET_PATH
             ann_path = os.path.join(data_path, "annotation")
-            self.crop_img = os.path.join(data_path, "crop_img")
-            self.data_file = os.path.join(ann_path, "kp_valid")
             self.label_file = os.path.join(ann_path, "split")
         else:
-            self.data_file = cfg.KEYPOINT_FILE
             self.label_file = cfg.TRAIN_TEST_SPLIT
-            self.crop_img = cfg.CROP_IMAGE
-
+            
         self.model_type = cfg.MODEL.TYPE
         assert self.model_type in ["two_stream",
-                                   "rgb", "kp"], f"model type not support"
+                                   "rgb", "kp", "flow"], f"model type not support"
         self.img_size = cfg.MODEL.IMG_SIZE
         self.clip_len = cfg.CLIP_LENGTH
         self.cfg = cfg
@@ -54,8 +50,10 @@ class Data_loader:
     def clip_extract(self, line, extra_label, step=None):
         if not step:
             step = 1 if line["length"] <= 2*self.clip_len else 2
-
-        return [[line.name, i, i+self.clip_len, line["label"]]+extra_label for i in range(line["starts"], line["ends"]-self.clip_len, step)]
+        if self.model_type == "flow" and line["length"] > self.clip_len:
+            return  [[line.name, i, i+self.clip_len, line["label"]]+extra_label for i in range(line["starts"]+1, line["ends"]-self.clip_len, step)]
+        else:
+            return [[line.name, i, i+self.clip_len, line["label"]]+extra_label for i in range(line["starts"], line["ends"]-self.clip_len, step)]
 
     def construct_loader(self, split):
         data_type = self.cfg.DATA.DATA_TYPE
@@ -76,8 +74,6 @@ class Data_loader:
 
         dataset = Pain_dataset(
             clip_df_set,
-            self.data_file,
-            self.crop_img,
             self.img_size,
             self.model_type,
             self.cfg,
@@ -100,18 +96,23 @@ class Pain_dataset(Dataset):
     def __init__(
         self,
         label_list,
-        video_file,
-        crop_img,
         img_size,
         model_type,
         cfg,
         aug
     ):
 
+        if cfg.FORMAT_DATASET and cfg.DATASET_PATH != "":
+            data_path = cfg.DATASET_PATH
+            ann_path = os.path.join(data_path, "annotation")
+            self.crop_img = os.path.join(data_path, "crop_img")
+            self.video_file = os.path.join(ann_path, "kp_valid")
+            self.flow_img = os.path.join(data_path, "optical_flow")
+        else:
+            self.crop_img = cfg.CROP_IMAGE
+            self.video_file = cfg.KEYPOINT_FILE
+            self.flow_img = cfg.FLOW_IMAGE
         self.label_list = label_list
-        self.video_file = video_file
-        self.crop_img = crop_img
-
         self.img_size = img_size
         self.data_dict = {}
         self.bbox_col = ["x", "y", "w", "h"]
@@ -140,9 +141,9 @@ class Pain_dataset(Dataset):
         aug_para = utils.aug_param_builder(
             clip_inf, video_name) if self.aug else []
 
-        if self.model_type in ["two_stream", "rgb"]:
+        if self.model_type in ["two_stream", "rgb", "flow"]:
             frames = self.frames_builder(
-                clip_inf[self.bbox_col], video_name, aug_para
+                clip_inf[self.bbox_col], video_name, aug_para, stream="rgb"
             )
             input_data.append(frames)
 
@@ -152,11 +153,17 @@ class Pain_dataset(Dataset):
                 feature, aug_para
             )
             input_data.append(kps)
-
+        if self.model_type in ["flow"]:
+            flow_video_name = os.path.join(self.flow_img, file_name.split(".")[0])
+            frames = self.frames_builder(
+                clip_inf[self.bbox_col], flow_video_name, aug_para, stream="flow"
+            )
+            input_data.append(frames)
+        start_name = clip_inf.index[0]
         if self.cfg.DATA.DATA_TYPE in ["simple", "diff"]:
-            return input_data, int(clip[-1])
+            return input_data, int(clip[-1]), start_name
         elif self.cfg.DATA.DATA_TYPE == "aux":
-            return input_data, [int(clip[-2]), int(clip[-1])]
+            return input_data, [int(clip[-2]), int(clip[-1])], start_name
 
     def keypoints_builder(self, kp_inf, aug_para):
         if not aug_para:
@@ -167,11 +174,220 @@ class Pain_dataset(Dataset):
         kp_inf = utils.kp_normal(kp_inf)
         return torch.from_numpy(kp_inf.copy()).float()
 
-    def frames_builder(self, frame_inf, video_name, aug_para):
+    def frames_builder(self, frame_inf, video_name, aug_para, stream="rgb"):
         crop_threshold = self.cfg.DATA.CROP_THRESHOLD
         frames = []
-        mean = self.cfg.DATA.MEAN
-        std = self.cfg.DATA.STD
+        mean = self.cfg.DATA.MEAN if stream == "rgb" else self.cfg.DATA.MEAN_FLOW
+        std = self.cfg.DATA.STD if stream == "rgb" else self.cfg.DATA.STD_FLOW
+        for img_index in frame_inf.index:
+            line = frame_inf.loc[img_index]
+            path = os.path.join(video_name, img_index)
+            img = Image.open(path)
+            img_w, img_h = img.size
+
+            crop_size = np.ceil(
+                max(line[["w", "h"]])/10
+            )*10
+            bound = max((img_w, img_h))
+            if crop_size <= bound:
+                crop_size = bound
+            crop_x = np.clip(
+                        line["x"] - (crop_size-line["w"]) /
+                        2, 0, img_w - crop_size
+                    )
+            crop_y = np.clip(
+                line["y"] - (crop_size-line["h"]) /
+                2, 0, img_h - crop_size
+            )
+            img = img.crop(
+                (crop_x, crop_y, crop_x+crop_size, crop_y+crop_size)
+            )
+            '''
+            for crop_size in crop_threshold:
+                if (line[["w", "h"]] < crop_size).all() and img_w != crop_size:
+                    crop_x = np.clip(
+                        line["x"] - (crop_size-line["w"]) /
+                        2, 0, img_w - crop_size
+                    )
+                    crop_y = np.clip(
+                        line["y"] - (crop_size-line["h"]) /
+                        2, 0, img_h - crop_size
+                    )
+                    img = img.crop(
+                        (crop_x, crop_y, crop_x+crop_size, crop_y+crop_size)
+                    )
+                    break
+            '''
+            img = img.resize(self.img_size, Image.ANTIALIAS)
+            frames.append(img)
+
+        frames = torch.as_tensor(np.stack(frames))
+        frames = utils.tensor_normalize(frames, mean, std)
+        frames = frames.permute(0, 3, 1, 2)
+
+        if not aug_para:
+            return frames
+        else:
+            return utils.frame_augmentation(frames, aug_para)
+
+
+class Dog_pain(torch.utils.data.Dataset):
+    def __init__(self, cfg, mode, num_retries=10):
+        assert mode in [
+            "train",
+            "val",
+            "test",
+        ], "Split '{}' not supported for Kinetics".format(mode)
+        self.mode = mode
+        self.cfg = cfg
+
+        self._video_meta = {}
+        self._num_retries = num_retries
+        # For training or validation mode, one single clip is sampled from every
+        # video. For testing, NUM_ENSEMBLE_VIEWS clips are sampled from every
+        # video. For every clip, NUM_SPATIAL_CROPS is cropped spatially from
+        # the frames.
+        '''
+        if self.mode in ["train", "val"]:
+            self._num_clips = 1
+        elif self.mode in ["test"]:
+            self._num_clips = (
+                cfg.TEST.NUM_ENSEMBLE_VIEWS * cfg.TEST.NUM_SPATIAL_CROPS
+            )
+        '''
+        logger.info("Constructing Kinetics {}...".format(mode))
+        self._construct_loader()
+        self.aug = False
+        self.rand_erase = False
+        self.use_temporal_gradient = False
+        self.temporal_gradient_rate = 0.0
+        self.clip_len = self.cfg.DATA.NUM_FRAMES
+        if self.mode == "train" and self.cfg.AUG.ENABLE:
+            self.aug = True
+            if self.cfg.AUG.RE_PROB > 0:
+                self.rand_erase = True
+    
+    def _construct_loader(self):
+        data_path = self.cfg.DATA.PATH_TO_DATA_DIR
+        assert os.path.isdir(data_path), f"{data_path} not found"
+        ann_path = os.path.join(data_path, "annotation")
+        self.crop_img = os.path.join(data_path, "crop_img")
+        self.video_file = os.path.join(ann_path, "kp_valid")
+        self.label_file = os.path.join(ann_path, "split")
+        
+        self.data_dict = {}
+        self.bbox_col = ["x", "y", "w", "h"]
+        self.clip_set = self.label_prepare()
+    
+    def label_prepare(self):
+        file = os.path.join(self.label_file, self.mode+".h5")
+        step = self.clip_len if self.mode == "test" else None
+        container = []
+
+        df = pd.read_hdf(file, "df_with_missing")
+        df = df.applymap(lambda item: int(item))
+        
+        for i in range(len(df)):
+            line = df.iloc[i]
+            clip_set = self.clip_extract(line, step)
+            container.extend(clip_set)
+        return container
+    
+    def clip_extract(self, line, step=None):
+        if not step:
+            step = 1 if line["length"] <= 2*self.clip_len else 2
+        return [[line.name, i, i+self.clip_len, line["label"]] for i in range(line["starts"], line["ends"]-self.clip_len, step)]
+        
+    def __len__(self):
+        return len(self.clip_set)
+    
+    def __getitem__(self, index):
+
+        short_cycle_idx = None
+        # When short cycle is used, input index is a tupple.
+        if isinstance(index, tuple):
+            index, short_cycle_idx = index
+
+        if self.mode in ["train", "val"]:
+            # -1 indicates random sampling.
+            
+            spatial_sample_index = -1
+            min_scale = self.cfg.DATA.TRAIN_JITTER_SCALES[0]
+            max_scale = self.cfg.DATA.TRAIN_JITTER_SCALES[1]
+            crop_size = self.cfg.DATA.TRAIN_CROP_SIZE
+            if short_cycle_idx in [0, 1]:
+                crop_size = int(
+                    round(
+                        self.cfg.MULTIGRID.SHORT_CYCLE_FACTORS[short_cycle_idx]
+                        * self.cfg.MULTIGRID.DEFAULT_S
+                    )
+                )
+        elif self.mode in ["test"]:
+            
+            # spatial_sample_index is in [0, 1, 2]. Corresponding to left,
+            # center, or right if width is larger than height, and top, middle,
+            # or bottom if height is larger than width.
+            spatial_sample_index = (
+                (
+                    self._spatial_temporal_idx[index]
+                    % self.cfg.TEST.NUM_SPATIAL_CROPS
+                )
+                if self.cfg.TEST.NUM_SPATIAL_CROPS > 1
+                else 1
+            )
+            min_scale, max_scale, crop_size = (
+                [self.cfg.DATA.TEST_CROP_SIZE] * 3
+                if self.cfg.TEST.NUM_SPATIAL_CROPS > 1
+                else [self.cfg.DATA.TRAIN_JITTER_SCALES[0]] * 2
+                + [self.cfg.DATA.TEST_CROP_SIZE]
+            )
+            # The testing is deterministic and no jitter should be performed.
+            # min_scale, max_scale, and crop_size are expect to be the same.
+            assert len({min_scale, max_scale}) == 1
+        else:
+            raise NotImplementedError(
+                "Does not support {} mode".format(self.mode)
+            )
+        
+        clip = self.clip_set[index]
+        file_name = clip[0]
+        if file_name in self.data_dict:
+            video_df = self.data_dict[file_name]
+
+        else:
+            video_df = pd.read_hdf(
+                os.path.join(self.video_file, file_name), "df_with_missing"
+            )
+            self.data_dict[file_name] = video_df
+
+        video_name = os.path.join(self.crop_img, file_name.split(".")[0])
+        clip_inf = video_df.iloc[clip[1]:clip[2]]
+        frames = self.frames_builder(
+                clip_inf[self.bbox_col], video_name
+            )
+        frames = utils.tensor_normalize(
+                    frames, self.cfg.DATA.MEAN, self.cfg.DATA.STD
+                )
+                # T H W C -> C T H W.
+        frames = frames.permute(3, 0, 1, 2)
+        if self.aug:
+            frames = utils.spatial_sampling(
+                    frames,
+                    spatial_idx=spatial_sample_index,
+                    min_scale=min_scale,
+                    max_scale=max_scale,
+                    crop_size=crop_size,
+                    random_horizontal_flip=self.cfg.DATA.RANDOM_FLIP,
+                    inverse_uniform_sampling=self.cfg.DATA.INV_UNIFORM_SAMPLE,
+                )
+        label = int(clip[-1])
+        frames = utils.pack_pathway_output(self.cfg, frames)
+        return frames, label, index, {}
+
+    def frames_builder(self, frame_inf, video_name):
+        crop_threshold = self.cfg.DATA.CROP_THRESHOLD
+        frames = []
+        
         for img_index in frame_inf.index:
             line = frame_inf.loc[img_index]
             path = os.path.join(video_name, img_index)
@@ -196,10 +412,8 @@ class Pain_dataset(Dataset):
             frames.append(img)
 
         frames = torch.as_tensor(np.stack(frames))
-        frames = utils.tensor_normalize(frames, mean, std)
-        frames = frames.permute(0, 3, 1, 2)
+        return frames
 
-        if not aug_para:
-            return frames
-        else:
-            return utils.frame_augmentation(frames, aug_para)
+        
+        
+       
